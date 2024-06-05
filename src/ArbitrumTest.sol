58e976c6eb4349aee6ff49c2aeab3fc7d7daa076
Tomás Rojas <tmsrjs@gmail.com>
2024-06-05 17:43:25 -0300

    refactor: move mocks setup to their own functions

--- src/ArbitrumTest.sol
+++ src/ArbitrumTest.sol
@@ -16,15 +16,25 @@ contract ArbitrumTest is TestBase {
     address public constant ARBSYS_PRECOMPILE = 0x0000000000000000000000000000000000000064;
 
     constructor() {
+        setUpArbSysMock();
+        setUpInboxMock();
+        setUpBridge();
+    }
+
+    function setUpArbSysMock() internal {
         // L2 contracts explicitly reference 0x64 for the ArbSys precompile
         // We'll replace it with the mock contract where L2-to-L1 messages are executed immediately
         arbsys = new ArbSysMock();
         vm.etch(ARBSYS_PRECOMPILE, address(arbsys).code);
+    }
 
+    function setUpInboxMock() internal {
         // use the mocked Arbitrum inbox where L1-to-L2 messages are executed immediately
         uint256 MAX_DATA_SIZE = 117_964;
         inbox = new ArbitrumInboxMock(MAX_DATA_SIZE);
+    }
 
+    function setUpBridge() internal {
         bridge = Bridge(arbsys.MAINNET_BRIDGE());
     }
 }
